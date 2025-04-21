5521b860f0f00c7787f6e5b5acd9faab23ca2bdb
Ashrith1307 <devarapallyashrithreddy@gmail.com>
2025-04-22 00:26:29 +0530

    Building Backend Completed

--- server/app.js
+++ server/app.js
@@ -1,33 +1,83 @@
 const express = require("express");
-const app = express();
-let tasks = [];
+const mongoose = require("mongoose");
+const tasks = require("./models/tasks");
 
+const app = express();
 app.use(express.json()); //Middleware
 
-app.get("/", (req, res) => {
-  res.send("TASK MANAGER");
-});
+// MongoDB Connection
+mongoose
+  .connect(
+    "mongodb+srv://devarapallyashrithreddy:13-01-2007@cluster0.w6pgok9.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDb is live");
+  })
+  .catch(() => {
+    console.log("Error in starting MongoDb");
+  });
 
-app.get("/about", (req, res) => {
-  res.send(
-    "This Task Manager is differnt from other because it uses the user authentication"
-  );
-});
-let id = 1;
-app.post("/tasks", (req, res) => {
+// inserting a task
+app.post("/tasks", async (req, res) => {
   const { taskTitle } = req.body;
-  const newTask = {
-    Uniqueid: id++,
-    title: taskTitle,
-    status: false,
-  };
-  tasks.push(newTask);
-  console.log(tasks);
-  res.send(`Task added : ${taskTitle}`);
+  try {
+    const newtask = tasks({ title: taskTitle });
+    await newtask.save();
+    res.status(200).json({ message: "Added task : ", taskTitle });
+  } catch (err) {
+    res.status(500).json({ message: "Error in Adding task: ", err });
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  res.json(tasks);
+// getting all tasks from mongoDb
+app.get("/tasks", async (req, res) => {
+  try {
+    const alltasks = await tasks.find();
+    res.status(200).json(alltasks);
+  } catch (err) {
+    res.status(500).json({ message: "Error in getting tasks :", err });
+  }
+});
+
+// Deleting a task by the id
+// after ":": is known as "params"
+app.delete("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const D_task = await tasks.findByIdAndDelete(id);
+    if (!D_task) {
+      return res.status(404).json({ message: "Task not Found" });
+    }
+    res.status(200).json({ message: `Task ${D_task.title} deleted` });
+  } catch (error) {
+    res.status(500).json({ message: "Error" });
+  }
+});
+
+// updating a task status
+app.patch("/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { completed } = req.body;
+
+  try {
+    const Upd_tasks = await tasks.findByIdAndUpdate(
+      id,
+      { completed: completed },
+      { new: true }
+    );
+    if (!Upd_tasks) {
+      res.status(404).json({ message: "Task not Found" });
+    }
+    res.status(200).json({
+      message: `task updated successfully`,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error in Updating the task" });
+  }
 });
 //Server live on 5000
 app.listen(5000, () => {
